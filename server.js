80d3d20cf699a351ead0f63e2f76e40a485bcb79
Chamidu Lakshan <lakshanchamidu994@gmail.com>
2025-10-23 22:26:16 +0530

    5

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3001', // frontend URL
+  origin: ['http://localhost:3001', 'http://localhost:3002', 'http://localhost:3003'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
