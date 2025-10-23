6eca054a96228832e04236a45ed2e5c86dd125fe
Chamidu Lakshan <lakshanchamidu994@gmail.com>
2025-10-23 23:08:07 +0530

    1

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:3001', 'http://localhost:3002', 'https://serviceadmin-app.netlify.app/'],
+  origin: ['https://servicecustomer-app.netlify.app', 'https://servicecoordinator-app.netlify.app', 'https://serviceadmin-app.netlify.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true
 }));
