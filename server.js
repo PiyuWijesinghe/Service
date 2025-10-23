a82db2b42326b60e375f709f270db1549a48760d
Chamidu Lakshan <lakshanchamidu994@gmail.com>
2025-10-23 21:52:33 +0530

    up

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const workerRoutes = require('./routes/worker.routes');
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3001', // frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 const connectDB = async () => {
     try {
