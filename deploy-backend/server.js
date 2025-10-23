c27aa059a3dc199943a6a067e489887845534282
Chamidu Lakshan <lakshanchamidu994@gmail.com>
2025-10-23 20:56:04 +0530

    init commit

---
+++ deploy-backend/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const requestRoutes = require('./routes/service.routes');
+const authRoutes = require('./routes/auth.routes');
+const workerRoutes = require('./routes/worker.routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const connectDB = async () => {
+    try {
+        const mongoURI = process.env.MONGO_URI;
+        await mongoose.connect(mongoURI);
+        console.log('MongoDB Connected to Service Application.');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+connectDB();
+
+app.use('/api/v1/requests', requestRoutes);
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/workers', workerRoutes);
+
+const PORT = process.env.PORT || 5002;
+
+app.listen(PORT, () => {
+    console.log(`Request Service running on port ${PORT}`);
+});
